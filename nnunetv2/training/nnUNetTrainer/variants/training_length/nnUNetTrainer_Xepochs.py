51ff7661449719e4903f687ea21819b343072d41
marwankefah <marwankefah@gmail.com>
2024-04-06 19:28:01 +0200

    added trainer for 1000 epochs.

--- nnunetv2/training/nnUNetTrainer/variants/training_length/nnUNetTrainer_Xepochs.py
+++ nnunetv2/training/nnUNetTrainer/variants/training_length/nnUNetTrainer_Xepochs.py
@@ -55,6 +55,12 @@ class nnUNetTrainer_250epochs(nnUNetTrainer):
         self.num_epochs = 250
 
 
+class nnUNetTrainer_1000epochs(nnUNetTrainer):
+    def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict, unpack_dataset: bool = True,
+                 device: torch.device = torch.device('cuda')):
+        super().__init__(plans, configuration, fold, dataset_json, unpack_dataset, device)
+        self.num_epochs = 1000
+
 class nnUNetTrainer_2000epochs(nnUNetTrainer):
     def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict, unpack_dataset: bool = True,
                  device: torch.device = torch.device('cuda')):
