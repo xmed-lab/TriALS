f5b6702c7f1e015b0f896c44f9a68b7196e6b591
marwankefah <marwankefah@gmail.com>
2024-08-08 04:01:24 +0300

    added docker submission instruction.

--- docker/template/src/Model.py
+++ docker/template/src/Model.py
@@ -2,9 +2,11 @@ import torch
 import os
 from nnunetv2.inference.predict_from_raw_data import nnUNetPredictor
 # predict a numpy array
+import numpy as np
+
 
 class MySegmentation:
-    def __init__(self, task="Dataset102_TriALS",nnunet_model_dir='nnUNet_results',
+    def __init__(self, task="Dataset102_TriALS", nnunet_model_dir='nnUNet_results',
                  model_name='nnUNetTrainer__nnUNetPlans__3d_fullres',
                  folds=(0, 1, 2, 3, 4)
                  ):
@@ -21,7 +23,7 @@ class MySegmentation:
         )
         self.predictor.initialize_from_trained_model_folder(
             os.path.join(nnunet_model_dir,
-                 f'{task}/{model_name}'),
+                         f'{task}/{model_name}'),
             use_folds=folds,
             checkpoint_name='checkpoint_final.pth',
         )
@@ -29,4 +31,7 @@ class MySegmentation:
     def process_image(self, image_np, properties):
         ret = self.predictor.predict_single_npy_array(
             image_np, properties, None, None, False)
+
+        # the lesion class should be set to 1 if it is not and others to 0
+        ret = (ret == 2).astype(np.uint8)
         return ret
